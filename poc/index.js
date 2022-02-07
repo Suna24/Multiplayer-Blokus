b94875725cc9e022ad509f7c0475b8fd3887de73
Suna24 <bastien.t2002@gmail.com>
2022-02-07 10:30:18 -0500

    Gestion de la connection de 4 joueurs avec des id #3

--- poc/index.js
+++ poc/index.js
@@ -1,23 +1,39 @@
 //Importations utiles
 const WebSocket = require('ws');
+var nombreDeJoueurs = 0;
 
+//Création du serveur
 const wss = new WebSocket.Server({port:3000}, () =>{
     console.log("Le serveur a bien démarré");
 })
 
+//Écoute du port
 wss.on('listening', () => {
     console.log("Le serveur écoute sur le port 3000");
 })
 
-wss.on('connection', (ws) => {
-    console.log("Connection effectuée");
+//Gestion des connections au serveur
+wss.on('connection', (ws, req) => {
 
-    ws.on('close', () => {
-        console.log("Déconnection effectuée");
-    })
+    if(nombreDeJoueurs < 4){
+
+        var id = req.headers['sec-websocket-key'];
+        console.log("Connection effectuée par " + id);
+
+        nombreDeJoueurs = nombreDeJoueurs + 1;
+
+        ws.on('close', () => {
+            console.log("Déconnection effectuée par " + id);
+            nombreDeJoueurs = nombreDeJoueurs - 1;
+        })
+
+    } else {
+        console.log("Serveur full, veuillez attendre une déconnection");
+    }
 
 })
 
+//Gestion de la fermeture du serveur
 wss.on('close', (ws) => {
     console.log("Le serveur a bien été éteint");
 })
