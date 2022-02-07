c6488c295db6c33fb3ce953ef978399566488e18
Suna24 <bastien.t2002@gmail.com>
2022-02-07 08:42:23 -0500

    Gestion de la connection par un client Unity #2

---
+++ poc/POC/Assets/Code/WS_Client.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using WebSocketSharp;
+
+public class WS_Client : MonoBehaviour
+{
+
+    WebSocket webSocket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        webSocket = new WebSocket("ws://localhost:3000");
+        webSocket.Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
