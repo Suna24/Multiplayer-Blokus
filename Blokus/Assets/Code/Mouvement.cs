c2897b9e3e71d3150ff20a979d4e1965f3c6349a
Suna24 <bastien.t2002@gmail.com>
2022-02-25 21:36:48 -0500

    Reviens à sa position quand placé en dehors du plateau #8

--- Blokus/Assets/Code/Mouvement.cs
+++ Blokus/Assets/Code/Mouvement.cs
@@ -4,15 +4,18 @@ public class Mouvement : MonoBehaviour
 {
 
     bool estEnMain = false;
+    bool doitRevenirASaPositionDOrigine = false;
     int n = 0;
     float xPositionInitiale, yPositionInitiale;
     Piece piece = new Piece();
+    Quaternion rotationOriginale;
 
     // Start is called before the first frame update
     void Start()
     {
         xPositionInitiale = transform.position.x;
         yPositionInitiale = transform.position.y;
+        rotationOriginale = transform.rotation;
     }
 
     // Update is called once per frame
@@ -24,12 +27,20 @@ public class Mouvement : MonoBehaviour
             Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector2(Mathf.Round(cursorPos.x) - 0.5f, Mathf.Round(cursorPos.y) - 0.5f);
 
-
             if (Input.GetMouseButtonDown(1))
             {
                 transform.Rotate(0, 0, 90, Space.World);
             }
 
+        }
+
+        if (doitRevenirASaPositionDOrigine == true)
+        {
+            transform.position = new Vector2(xPositionInitiale, yPositionInitiale);
+            estEnMain = false;
+            piece.estPosee = false;
+            doitRevenirASaPositionDOrigine = false;
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotationOriginale, Time.time * 1.0f);
         }
     }
 
@@ -38,11 +49,18 @@ public class Mouvement : MonoBehaviour
         if (piece.estPosee == false)
         {
             n++;
-
             if (n % 2 == 0)
             {
-                estEnMain = false;
-                piece.estPosee = true;
+                if (transform.position.x >= -10 && transform.position.x <= 10 && transform.position.y >= -12 && transform.position.y <= 8)
+                {
+                    estEnMain = false;
+                    piece.estPosee = true;
+                }
+                else
+                {
+                    Debug.Log("Ne peut pas être placée ici");
+                    doitRevenirASaPositionDOrigine = true;
+                }
             }
             else
             {
