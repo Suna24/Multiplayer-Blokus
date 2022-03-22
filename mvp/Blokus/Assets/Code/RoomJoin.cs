65371551e0b1ddbaea21f85032c8043ca01d6f84
Suna24 <bastien.t2002@gmail.com>
2022-03-22 18:07:44 -0400

    affichage des rooms disponibles dans la scroll view #11

--- mvp/Blokus/Assets/Code/RoomJoin.cs
+++ mvp/Blokus/Assets/Code/RoomJoin.cs
@@ -1,33 +1,75 @@
 using UnityEngine;
 using UnityEngine.UI;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 public class RoomJoin : MonoBehaviour
 {
 
     WebSocketClient webSocketClient;
+    [SerializeField] private Transform contenuContainer;
+    [SerializeField] private GameObject prefab;
+    string[] noms;
+    int[] nbJoueursCourant;
+    int[] nbJoueursTotal;
+    bool aFini = false;
+    int index = 0;
+
 
     public void Start()
     {
-        webSocketClient = WebSocketClient.getInstance();
+        webSocketClient = WebSocketClient.webSocketClient;
 
         //Listener pour la réponse du serveur
         webSocketClient.GetWebSocket().OnMessage += (sender, e) =>
         {
-            Debug.Log("dans Listener");
             Debug.Log(e.Data);
 
             Message.MessageRooms messageRooms = JsonConvert.DeserializeObject<Message.MessageRooms>(e.Data);
 
+            int taille = messageRooms.rooms.GetLength(0);
+
+            noms = new string[taille];
+            nbJoueursCourant = new int[taille];
+            nbJoueursTotal = new int[taille];
+
             //Pour chaque room présente sur le serveur on les affiche sous forme de boutons
             foreach (Message.MessageRoom room in messageRooms.rooms)
             {
-                GameObject button = new GameObject();
-                button.AddComponent<Button>();
-                button.transform.position = new Vector3(192f, 122f, 0f);
-                button.GetComponent<Button>().GetComponent<Text>().text = room.nom + "      " + room.nbJoueursCourant + "/" + room.nbJoueursTotal + "joueurs";
+                Debug.Log(room.nom);
+                Debug.Log(room.nbJoueursCourant);
+                Debug.Log(room.nbJoueursTotal);
+
+                noms[index] = room.nom;
+                nbJoueursCourant[index] = room.nbJoueursCourant;
+                nbJoueursTotal[index] = room.nbJoueursTotal;
+
+                Debug.Log(index);
+
+                index++;
             }
+
+            aFini = true;
         };
+
+    }
+
+    public void Update()
+    {
+
+        if (aFini)
+        {
+
+            for (int i = 0; i < noms.Length; i++)
+            {
+                GameObject button = Instantiate(prefab, new Vector3(1f, 1f, 1f), Quaternion.identity);
+                button.GetComponentInChildren<Text>().text = noms[i] + "        " + nbJoueursCourant[i] + "/" + nbJoueursTotal[i] + " joueurs";
+                button.transform.localScale = Vector3.one;
+                button.transform.SetParent(contenuContainer, false);
+            }
+
+            aFini = false;
+        }
 
     }
 
